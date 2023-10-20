a3fea55db93254327fb2bd347a6effcc0afd1d2e
asilbeksagdullayev <asilbek1188akeng@gmail.com>
2023-10-20 21:46:22 +0500

    second_codes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
 import './projects/pay-me/typescript';
+import "./main.css";
+
+
+const userValue:HTMLInputElement = document.querySelector(".firstInput")!;
+const cardsValue:HTMLInputElement = document.querySelector(".secondInput")!;
+const cardError:HTMLParagraphElement = document.querySelector(".error")!;
+const userError:HTMLParagraphElement = document.querySelector(".error1")!;
+const btn: HTMLButtonElement = document.querySelector(".btn")!;
+
+btn.addEventListener("click", () => {
+if(userValue.value !== "") {
+
+ userValue.value = "";
+} else {
+ userError.style.display = "block";
+ userError.innerText = "Please enter number";
+ userError.style.color = "red";
+
+ setTimeout(() => {
+  userError.style.display = "none";
+}, 3000);
+}
+if(cardsValue.value === "") {
+ cardError.innerText = "Please enter number";
+ cardError.style.color = "red";
+ setTimeout(() => {
+   cardError.style.display = "none";
+ }, 3000);
+} else {
+ cardsValue.value = "333";
+}
+});
