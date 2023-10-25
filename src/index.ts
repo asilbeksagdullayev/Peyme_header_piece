74ec43d3ca892b5056fa3877e4cf2ea6f0894af1
asilbeksagdullayev <asilbek1188akeng@gmail.com>
2023-10-25 12:08:45 +0500

    third_codes

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,18 @@
 import './projects/pay-me/typescript';
 import "./main.css";
 
-
-const userValue:HTMLInputElement = document.querySelector(".firstInput")!;
-const cardsValue:HTMLInputElement = document.querySelector(".secondInput")!;
-const cardError:HTMLParagraphElement = document.querySelector(".error")!;
-const userError:HTMLParagraphElement = document.querySelector(".error1")!;
+const userValue: HTMLInputElement = document.querySelector(".firstInput")!;
+const cardsValue: HTMLInputElement = document.querySelector(".secondInput")!;
+const cardError: HTMLParagraphElement = document.querySelector(".error")!;
+const userError: HTMLParagraphElement = document.querySelector(".error1")!;
 const btn: HTMLButtonElement = document.querySelector(".btn")!;
+const first_page: HTMLDivElement = document.querySelector(".first_page")!;
 
 btn.addEventListener("click", () => {
-if(userValue.value !== "") {
-
- userValue.value = "";
-} else {
- userError.style.display = "block";
- userError.innerText = "Please enter number";
- userError.style.color = "red";
-
- setTimeout(() => {
-  userError.style.display = "none";
-}, 3000);
-}
-if(cardsValue.value === "") {
- cardError.innerText = "Please enter number";
- cardError.style.color = "red";
- setTimeout(() => {
-   cardError.style.display = "none";
- }, 3000);
-} else {
- cardsValue.value = "333";
-}
+  if (userValue.value !== "") {
+    first_page.style.display = "none";
+    // create_container()
+  }
 });
+
+// create_container();
